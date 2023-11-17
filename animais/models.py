1d2e49a8d085297d917effd58b26ff3312090d3b
David <74156657+David-santana01@users.noreply.github.com>
2023-11-17 17:21:20 -0300

    criei a class animal

--- animais/models.py
+++ animais/models.py
@@ -3,4 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Animais(models.Model):
-    pass
+    nome = models.CharField(max_length=100)
+    raça = models.CharField(max_length=100)
+    idade = models.IntegerField(decimal_places=0 max_digits=20)
+    adotado = models.BooleanField(default=False)
+     
